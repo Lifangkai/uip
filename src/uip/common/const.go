45188745c617518b81f3ba622fb5b10d858124bb
quasimodo <quasimodo7614@163.com>
2018-04-24 16:07:07 +0800

    before commit

--- src/uip/common/const.go
+++ src/uip/common/const.go
@@ -57,4 +57,16 @@ const (
 
 	ErrorParameterIsErrId  = "200014"
 	ErrorParameterIsErrMsg = "参数错误"
+
+	ErrorJsonFmtErrId = "2000015"
+	ErrorJsonFmtErrMsg = "json格式错误!"
+
+	ErrorDataNullId = "2000016"
+	ErrorDataNullMsg = "请求参数为空!"
+
+	ErrorGroupIdLenId = "2000017"
+	ErrorGroupIdLenMsg ="机构表示长度不为8位！"
+
+	ErrorInterLenId = "2000018"
+	ErrorInterLenMsg = "接口编码长度大于32位！"
 )
