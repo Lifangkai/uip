7c206a52565d57c78c6660e610fdb0bb6c8e0f09
quasimodo <quasimodo7614@163.com>
2018-06-13 10:58:47 +0800

    "子表interfunc 增加inteCode作为主键"

--- src/uip/uip/parameterFillter.go
+++ src/uip/uip/parameterFillter.go
@@ -6,7 +6,7 @@ import (
 )
 func UipSrcInterfaceParaFilter(data attached.UipSrcInterface) (emptyCheckResponse responseJson, isEmpty bool) {
 	isEmpty = true
-	if len(data.GroupId) == 0 || len(data.InteCode)==0{
+	if len(data.GroupId) == 0 || len(data.InteCode)==0 || len(data.InteCode) == 0{
 		emptyCheckResponse.Code = common.ErrorDataNullId
 		emptyCheckResponse.Msg = common.ErrorDataNullMsg
 		isEmpty = false
