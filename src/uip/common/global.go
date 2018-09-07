35084f02b5f31dc7c700caa8e0acb4154cb9d288
372323556@qq.com <372323556@qq.com>
2018-09-07 16:51:43 +0800

    add global file

---
+++ src/uip/common/global.go
@@ -0,0 +1,8 @@
+package common
+
+import (
+  "rsf/rsf"
+)
+
+//定义全局变量指针Rsf
+var Frame *rsf.Rsf
---
+++ webRoot/src/components/Global.vue
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+const GroupId = "SNS16494"
+
+//const colorListLength = 20
+// function getGroupId () { 
+//   return GroupId
+// }
+// 
+// function setGroupId (aGroupId) { 
+//   GroupId = aGroupId
+// }
+
+export default
+{
+  GroupId 
+//   getGroupId,
+//   setGroupId
+}
+</script>
